9025dea37ceb56de3da4e4c91b05826dc92004c8
tanishk <184149842+tan22-max@users.noreply.github.com>
2026-04-01 17:54:31 +0000

    converstionofdatatypes

---
+++ 01_baics/cnversionoperation.js
@@ -0,0 +1,33 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); // 33aaNAN - > speficific type of numbet 
+// "33" => 33
+// "33aa" => NaN
+// null -> 0 
+// bollean - true-1 ., false-0
+
+let isLoggedin = "hitrsg";
+let bolleanisLoggedin = Boolean(isLoggedin);
+//console.log(bolleanisLoggedin);
+// 1-> true ; 0->false
+//"" -> false;
+// "skgr" -> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
