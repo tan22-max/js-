6399e59bd46ecdeca1b9d70e0b972636b986c2c3
tanishk <184149842+tan22-max@users.noreply.github.com>
2026-04-01 17:36:29 +0000

    datatype

---
+++ 01_baics/01_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // it tells to use tth newer version for all js code in our doc;
+// alert(3+3); // shows error in node js (here) as it woeks shows pop in browser 
+
+// code redeablity should be high:
+let name = "tanishk"
+let age = 3
+let islogedin = true
+
+/*
+types of datatypes
+1) string 
+2) number 2 to power 53
+3) boolean - true or false
+4) null => staqandalone value
+5) undefined => 
+5) symbol => unique
+
+object
+ 
+
+*/
+console.log(typeof age)
+console.log(typeof undefined);
+console.log(typeof null);
+
