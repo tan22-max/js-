999f07bf59b931a7ca6ec618f9463481dd6c03c8
tanishk <184149842+tan22-max@users.noreply.github.com>
2026-04-03 12:53:03 +0000

    number_math

---
+++ 01_baics/07_num_maths.js
@@ -0,0 +1,51 @@
+const score = 400 // auto detects nuumber js\
+const balance = new Number(400)
+console.log(score)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+const othernumber  =  123.34
+
+console.log(othernumber.toPrecision(3));
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+ 
+// ********************************************************************************************
+
+console.log(Math);
+console.log(Math.abs(-3));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+// math.min , math.max 
+
+console.log(Math.random()); // gives random vakues 0 to1
+
+// need 1 to 10;
+
+console.log((Math.random()*10)+1);
+// formula 
+const min = 10;
+const max = 20;
+
+console.log((Math.random()*(max-min+1))+min);
+console.log((Math.random()*(max-min+1))+min);
+console.log((Math.random()*(max-min+1))+min);
+console.log((Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
