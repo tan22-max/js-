62ca77df5ee9f05bc26dde4a323ad04f76109a03
tanishk <184149842+tan22-max@users.noreply.github.com>
2026-04-03 12:52:39 +0000

    strings

---
+++ 01_baics/06_strings.js
@@ -0,0 +1,46 @@
+const name = "tanishk"
+const repocount = 50;
+
+ // console.log(name + repocount + " values"); // recomended to not use in real . as not very good and view friendlt
+
+ console.log(`my name is ${name} and my repocount is ${repocount} values`);
+ const gameName = new String('tanishk-hddf-com');
+ console.log(gameName);
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(0));
+ console.log(gameName.indexOf('t'));
+
+ const newstring = gameName.substring(0,3) // cant use negattive values 
+ console.log(newstring);
+ const anotherstring = gameName.slice(-8,4);
+ console.log(anotherstring)
+
+ const newstringone = "     tanishk       "
+ console.log(newstringone)
+ console.log(newstringone.trim());  // trim also has end and beginind functon
+
+ const url = "https://tanishk.com/hitesh%20chaudhary" // " " in brouser -> %20
+ console.log(url);
+ 
+ console.log(url.replace('%20','-'));
+ console.log(url.includes('sundar'));
+ console.log(gameName.split('-'));
+ 
+ 
+
+
+ 
+
+ 
+
+
+
+
+ 
+
+
+ 
+ 
+
+
