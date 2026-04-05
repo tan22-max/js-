7590723186c2045f5f21b37c66728fa625a0c531
tanishk <184149842+tan22-max@users.noreply.github.com>
2026-04-05 09:37:08 +0000

    object2

---
+++ 02_baics/04_objects.js
@@ -0,0 +1,91 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+// object can have objects  , can be excessed by multiplr . 
+// like loop
+console.log(regularUser.fullname.userfullname.firstname);
+// to merge mupleile objects 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+// assign 
+// const obj4 = Object.assign(obj1,obj2) // here target is o obj1 
+// const obj5 = Object.assign({},obj1,obj2) // here arget is {} , ADD OBJ 3 , OBJ4 TO IT
+
+
+// console.log(obj4)
+// console.log(obj5)
+
+
+const obj6 = {...obj1, ...obj2}
+console.log(obj6)
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+// its array inside object 
+users[1].email
+
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); // hasownproperty : tells if it has 
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+// api 
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
+
+
+
