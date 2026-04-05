243d6644c319c17e2f2d1ed8e161803f83d78bc0
tanishk <184149842+tan22-max@users.noreply.github.com>
2026-04-05 09:37:18 +0000

    function1

---
+++ 03_baics/01_functions.js
@@ -0,0 +1,22 @@
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+sayMyName()
+function ADDtwonumber(number1, number2){
+
+    //console.log(number1+number2);
+    // let result = number1+number2
+    // return result;
+    return number1+number2;
+
+    
+
+}
+const result = ADDtwonumber(2,3);
+console.log("result :" , result);
