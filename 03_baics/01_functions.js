8c7c0a745999445725fd6a894c57c524ed8803f0
tanishk <184149842+tan22-max@users.noreply.github.com>
2026-04-07 03:22:56 +0000

    funvtion

--- 03_baics/01_functions.js
+++ 03_baics/01_functions.js
@@ -20,3 +20,48 @@ function ADDtwonumber(number1, number2){
 }
 const result = ADDtwonumber(2,3);
 console.log("result :" , result);
+
+// part 2
+
+
+function loginUserMessage(username = "sam"){ // username - parameter - default = sam (if no argument)
+    if(!username){
+        console.log("PLease enter a username");
+        return  // never will utilse as ddefault is given 
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMessage())
+console.log(loginUserMessage("hitesh"))
+
+function calculateCartPrice(val1, val2, ...num1){ // if we wnat to give multipee inputs like values 
+    return num1
+}
+// val1 = 200 , val2 = 400 , num1 = [500,2000] 
+
+console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
+
